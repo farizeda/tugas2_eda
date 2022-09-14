11e4b78a335dbc323d1dda33ef638f8134c5452b
farizedaandhika21 <farizedaandhika21@gmail.com>
2022-09-14 17:03:29 +0700

    Finished

--- katalog/views.py
+++ katalog/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from katalog.models import CatalogItem
 
-# TODO: Create your views here.
+def show_katalog(request):
+    data_katalog = CatalogItem.objects.all()
+    context = {
+        'list_barang': data_katalog,
+        'nama': 'Muhammad Fariz Eda Andhika',
+        'npm': '2106653546'
+    }
+    return render(request, "katalog.html", context)
